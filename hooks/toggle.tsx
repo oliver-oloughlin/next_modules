0058eb696687bf40a1a60acce64fcc0a157dea5c
= <=>
2022-08-03 12:01:56 +0200

    Forceful typing of toggle hooks

--- hooks/toggle.tsx
+++ hooks/toggle.tsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 export function useToggle(startValue: boolean) {
     const [state, setState] = useState<boolean>(startValue)
     const toggleState = () => setState( val => !val )
-    return [state, toggleState]
+    return [state, toggleState] as [boolean, () => void]
 }
 
 export function useCycle<T>(values: T[]) {
     const [index, setIndex] = useState<number>(0)
     const cycleState = () => setIndex( i => i + 1 % values.length )
-    return [values[index], cycleState]
+    return [values[index], cycleState] as [T, () => void]
 }
